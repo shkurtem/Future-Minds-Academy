5e723e96d06932ecf802a6e030dc6136f0960771
shkurtem <shkurtemorin@gmail.com>
2024-04-18 17:31:57 +0200

    .

--- Assignments/JS/Quiz_Application_with_Timer_04-15-2024/js/script.js
+++ Assignments/JS/Quiz_Application_with_Timer_04-15-2024/js/script.js
@@ -5,9 +5,14 @@ const continue_btn  = document.querySelector("#continue_btn");
 const quiz_box  = document.querySelector(".quiz_box");
 const que_text  = document.querySelector(".que_text");
 const option_list = document.querySelector(".option_list");
-const timer_sec = document.querySelector(".timer_sec")
+const timer_sec = document.querySelector(".timer_sec");
+const time_line = document.querySelector (".time_line");
+const next_btn = document.querySelector (".next_btn");
+const total_que =document.querySelector (".total_que");
+
 
 let timer = 15;
+let lineTimer = 0;
 
 start_btn.addEventListener("click", function(){
     info_box.classList.add("activeInfo");
@@ -30,15 +35,22 @@ function loadQuestion(index){
     que_text.innerHTML = questions[index].question;
     
     for(let i=0; i<allOptions.length; i++){
-        option_list.innerHTML +=`<div class="option">
+        option_list.innerHTML +=`<div class="option "onclick = "optionSelect (this)">
                                     <span>${allOptions[i]}</span>
                                 </div>`;
     }
     let timerInterval = setInterval(timerCoint,1000);
 }
 let timerCoint = function (){
-    timer --;
+    if (timer > 0 ){ timer --}
+    
     timer_sec.textContent= timer;
+
+    lineTimer += 550 / 15;
+    time_line.style.width= lineTimer + "px";
 }
-
-
+let optionSelect = function(opt ){
+    console.log(opt);
+    next_btn.classList.add("show");
+   
+}
